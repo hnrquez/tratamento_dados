d605c01d809b54d4663e3f94560c33d4dabc5f6a
hnrquez <115908728+hnrquez@users.noreply.github.com>
2024-09-02 19:54:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+function testar(){
+    var nome = document.getElementById('nome');
+    if(nome.value.length==0){
+        window.alert('[ERRO] verifique os dados e tente novamente');
+    }else{
+        if(nome.value.length > 1 && nome.value[0]=='a'){
+            window.alert('Muito sapatona KKKKK');
+        }else{
+            if(nome.value.length > 1 && nome.value[0]=='b'){
+                window.alert('bisexual imunde');
+            }else{
+                if(nome.value.length > 1 && nome.value[0]=='c'){
+                    window.alert('hetero top');
+                }else{
+                    if(nome.value.length>1 && nome.value[0]=='d'){
+                        window.alert('gay demaisKKKK')
+                    }else{
+                        if(nome.value.length>1 && nome.value[0]== 'e'){
+                            window.alert('sapatona')
+                        }else{
+                            if(nome.value.length>1&&nome.value[0]=='f'){
+                                window.alert('o mais  viado')
+                            }else{
+                                if(nome.value.length>1&&nome.value[0]=='g'){
+                                    window.alert('sapatona cola velcro')
+                                }else{
+                                    if(nome.value.length>1&&nome.value[0]=='h'){
+                                        window.alert('sapatona chupa charque')
+                                    }else{
+                                        if(nome.value.length>1&&nome.value[0]=='i'){
+                                            window.alert('gay afeminade')
+                                        }else{
+                                            if(nome.value.length>1&&nome.value[0]=='j'){
+                                                window.alert('sapatona autista')
+                                            }else{
+                                                if(nome.value.length>1&&nome.value[0]=='l'){
+                                                    window.alert('hetero top')
+                                                }else{
+                                                    if(nome.value.length>1&&nome.value[0]=='m'){
+                                                        window.alert('hetero sigiloso')
+                                                    }else{
+                                                        if(nome.value.length>1&&nome.value[0]=='n'){
+                                                            window.alert('hetero que da o caneco')
+                                                        }else{
+                                                            if(nome.value.length>1&&nome.value[0]=='o'){
+                                                                window.alert('hetero gente normal')
+                                                            }else{
+                                                                if(nome.value.length>1&&nome.value[0]=='p'){
+                                                                    window.alert('cabra macho')
+                                                                }else{
+                                                                    if(nome.value.length>1&&nome.value[0]=='q'){
+                                                                        window.alert('da o caneco no sigilo')
+                                                                    }else{
+                                                                        if(nome.value.length>1&&nome.value[0]=='r'){
+                                                                            window.alert('gay que paga de hetero')
+                                                                        }else{
+                                                                            if(nome.value.length>1&&nome.value[0]=='s'){
+                                                                                window.alert('chupa charque')
+                                                                            }else{
+                                                                                if(nome.value.length>1&&nome.value[0]=='t'){
+                                                                                    window.alert('sapatona cola velcro')
+                                                                                }else{
+                                                                                    if(nome.value.length>1&&nome.value[0]=='u'){
+                                                                                        window.alert('lgbt evangelico')
+                                                                                    }else{
+                                                                                        if(nome.value.length>1&&nome.value[0]=='v'){
+                                                                                            window.alert('hetero')
+                                                                                        }else{
+                                                                                            if(nome.value.length>1&&nome.value[0]=='x'){
+                                                                                                window.alert('sapata satanista')
+                                                                                            }else{
+                                                                                                if(nome.value.length>1&&nome.value[0]=='y'){
+                                                                                                    window.alert('caminhoneira')
+                                                                                                }else{
+                                                                                                    if(nome.value.length>1&&nome.value[0]=='w'){
+                                                                                                        window.alert('viado')
+                                                                                                    }else{
+                                                                                                        if(nome.value.length>1&&nome.value[0]=='z'){
+                                                                                                            window.alert('gay dms')
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
+}
